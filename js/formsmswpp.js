4748618f6dd5ebc3f119ad4ace4978c6b496f0b0
DavidRGarnicaM <79337022+DavidRGarnicaM@users.noreply.github.com>
2021-03-31 15:57:07 -0600

    Add files via upload

---
+++ js/formsmswpp.js
@@ -0,0 +1,13 @@
+var feolo = document.getElementById("formsmswpp");
+function funsmswpp ()
+{
+    if (feolo.style.transform =="scale(1)")
+    {
+        feolo.style.transform ="scale(0)";
+    }
+    else
+    {
+        feolo.style.transform ="scale(1)";
+    }
+
+}
